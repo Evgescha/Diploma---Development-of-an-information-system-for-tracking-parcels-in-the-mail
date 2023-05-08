f0db0c2ad7e45d0c7e69b806f50ac5637a97d306
Yauhescha <evgescha.maslov007@yandex.ru>
2023-05-09 00:29:46 +0300

    fix menu accessing

--- src/main/java/com/hescha/mailtracking/service/ParcelService.java
+++ src/main/java/com/hescha/mailtracking/service/ParcelService.java
@@ -17,26 +17,6 @@ public class ParcelService extends CrudService<Parcel> {
         this.repository = repository;
     }
 
-    public Parcel findBySender(User sender) {
-        return repository.findBySender(sender);
-    }
-
-    public Parcel findByRecipient(User recipient) {
-        return repository.findByRecipient(recipient);
-    }
-
-    public Parcel findByWeight(double weight) {
-        return repository.findByWeight(weight);
-    }
-
-    public Parcel findByCost(double cost) {
-        return repository.findByCost(cost);
-    }
-
-    public List<Parcel> findByRoutesContains(com.hescha.mailtracking.model.Route routes) {
-        return repository.findByRoutesContains(routes);
-    }
-
     public Parcel update(Long id, Parcel entity) {
         Parcel read = read(id);
         if (read == null) {
